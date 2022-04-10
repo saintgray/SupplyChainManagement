e7ae143163f7b6b025f4f868c24a7c4807288a5b
unknown <saintgray0@gmail.com>
2022-04-10 17:54:44 +0900

    공지사항 권한 처리
    인가되지 않은 사용자는 공지사항 등록, 수정, 삭제를 할 수 없다 (Interceptor 설정)

---
+++ scmmar1/src/main/java/kr/happyjob/study/notice/interceptor/NoticeManageAuthCheckInterceptor.java
@@ -0,0 +1,27 @@
+package kr.happyjob.study.notice.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class NoticeManageAuthCheckInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		
+		boolean canManage=request.getSession().getAttribute("userType").equals("A");
+		if(!canManage){
+			
+			response.sendError(403);
+		}
+		return canManage;
+		
+	}
+	
+	
+	
+
+}
