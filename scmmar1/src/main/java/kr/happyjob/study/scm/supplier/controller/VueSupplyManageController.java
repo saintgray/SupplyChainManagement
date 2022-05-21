932e34adef65d9999e19e24727e4083418b31dbd
unknown <saintgray0@gmail.com>
2022-05-21 23:37:44 +0900

    Update VueSupplyManageController.java
    Conflict resolved

--- scmmar1/src/main/java/kr/happyjob/study/scm/supplier/controller/VueSupplyManageController.java
+++ scmmar1/src/main/java/kr/happyjob/study/scm/supplier/controller/VueSupplyManageController.java
@@ -1,17 +1,23 @@
 package kr.happyjob.study.scm.supplier.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.happyjob.study.scm.supplier.model.ImpSales;
 import kr.happyjob.study.scm.supplier.model.PageInfo;
 import kr.happyjob.study.scm.supplier.service.SupplyManageService;
 
@@ -19,16 +25,24 @@ import kr.happyjob.study.scm.supplier.service.SupplyManageService;
 @RequestMapping("/scm/vue")
 public class VueSupplyManageController {
 	
+
 	
 	private SupplyManageService supplyService;
-	private final Logger logger= LoggerFactory.getLogger(this.getClass());
+	private final Logger logger=LogManager.getLogger(this.getClass());
 	
+	
+	public VueSupplyManageController() {
+
+	}
+
+
 	@Autowired
 	public VueSupplyManageController(SupplyManageService supplyService) {
 		this.supplyService = supplyService;
 	}
 	
 	
+
 	@GetMapping("/suppliers")
 	public Map<String,Object> getSuppliers(PageInfo info,HttpServletResponse resp){
 		
@@ -45,6 +59,35 @@ public class VueSupplyManageController {
 	}
 	
 	
+
+	
+	@PostMapping("/supplier/{id}")
+	public Map<String,Object> getSupplierImpSales(Model model, @PathVariable(value="id") String idx){
+		
+		Map<String,Object> result = new HashMap<>();
+		
+		try{
+			List<ImpSales> importList=null;
+			importList=supplyService.getImpSales(idx);
+			result.put("impSales", importList);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	
+	@PostMapping("/supply/comnCod.do")
+	@ResponseBody
+	public Map<String, Object> getSupplyComnCod(){
+		Map<String, Object> result=new HashMap<String, Object>();
+		result.put("list", supplyService.getComnCod());
+		
+		return result;
+	}
+	
+
 	
 	
 	
